cbb1304bfd1a4b66ae56dfa28bf329f5027d5227
Dipesh Gautam <dipesh.gautam@solarisbank.de>
2021-08-27 23:53:08 +0200

    rename

--- go.mod
+++ go.mod
@@ -8,6 +8,5 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.6.0
 	github.com/dgtm/log360/logstreamer v0.0.0-20210827205808-109a03465c2c
 	google.golang.org/grpc v1.40.0
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/protobuf v1.27.1
 )
--- logstreamer/process.go
+++ logstreamer/process.go
@@ -3,16 +3,14 @@ package logstreamer
 import (
 	"context"
 	"fmt"
-
-	"github.com/dgtm/log360/logstreamer"
 )
 
 type Server struct{}
 
-func (*Server) ProcessRequest(context context.Context, req *logstreamer.LogRequest) (*LogResponse, error) {
+func (*Server) ProcessRequest(context context.Context, req *LogRequest) (*LogResponse, error) {
 	fmt.Println("Got a new Add request")
 	mins := req.GetMinutes()
 	// profiles := req.GetProfiles()
-	result := &logstreamer.LogResponse{Result: mins * 100}
+	result := &LogResponse{Result: mins * 100}
 	return result, nil
 }
